113c993512fac8faca0757e99a15603b340bfff1
JyotBrainBinary <jyot.brainbinary@gmail.com>
2024-05-28 18:16:28 +0530

    changes

--- lib/presentation/me_screen/screens/setting_screen/Page/account_screen/account_screen.dart
+++ lib/presentation/me_screen/screens/setting_screen/Page/account_screen/account_screen.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+
 import 'package:transform_your_mind/core/utils/dimensions.dart';
+import 'package:transform_your_mind/core/utils/extension_utils.dart';
 import 'package:transform_your_mind/core/utils/image_constant.dart';
+import 'package:transform_your_mind/core/utils/size_utils.dart';
 import 'package:transform_your_mind/widgets/custom_appbar.dart';
 
 class AccountScreen extends StatelessWidget {
@@ -14,12 +17,48 @@ class AccountScreen extends StatelessWidget {
       body: Stack(
         children: [
           Positioned(
-            top: Dimens.d120,
-            right: 0,
-            left:  null,
-            child: Image.asset(ImageConstant.bgStar, height: Dimens.d287),
+            top: Dimens.d200.h,
+            right: null,
+            left: 0,
+            child: Transform.rotate(
+              angle: 3.14,
+              child: Image.asset(ImageConstant.bgStar, height: Dimens.d274.h),
+            ),
           ),
-
+          // Column(
+          //   children: [
+          //     Dimens.d30.spaceHeight,
+          //     Container(
+          //       decoration: BoxDecoration(
+          //         color: AppColors.white,
+          //         borderRadius: BorderRadius.circular(Dimens.d16),
+          //       ),
+          //       child: ListView.separated(
+          //         physics: const NeverScrollableScrollPhysics(),
+          //         shrinkWrap: true,
+          //         clipBehavior: Clip.none,
+          //         padding: EdgeInsets.zero,
+          //         itemBuilder: (context, index) {
+          //           var data = accountData[index];
+          //           return SettingListItem(
+          //             isSettings: false,
+          //             prefixIcon: data.prefixIcon,
+          //             title: data.title,
+          //             suffixIcon: data.suffixIcon,
+          //             onTap: () {
+          //               _handleSettingsTap(index);
+          //             },
+          //           );
+          //         },
+          //         separatorBuilder: (context, index) => Padding(
+          //           padding: Dimens.d20.paddingHorizontal,
+          //           child: const DividerWidget(),
+          //         ),
+          //         itemCount: accountData.length,
+          //       ),
+          //     ),
+          //   ],
+          // ),
         ],
       ),
     );
